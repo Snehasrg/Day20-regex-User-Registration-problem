e879ded3c5ac43f870029bedaf0d11c1cb4a27a0
Snehasrg <sghongade642@gmail.com>
2023-01-20 18:51:32 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegistrarionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome To User Registration Program. ");
+
+        }
+    }
+}
