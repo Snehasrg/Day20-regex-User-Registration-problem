42da5c58210a6f0b97e80a232015ba8cd6a97a9c
Snehasrg <sghongade642@gmail.com>
2023-01-20 19:18:31 +0530

    Validate Email

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,1 +1,18 @@
 ﻿
+using UserRegistrationProblem;
+
+namespace RegexTestCase
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UseCase3()
+        {
+            string email = "abc.xyz@bl.co.in";
+            bool expected = true;
+            bool actual = Pattern.validateEmailID(email);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
