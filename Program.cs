5de2527f21b1011023ae4d642a9d6330b31b70cd
Snehasrg <sghongade642@gmail.com>
2023-01-20 19:00:03 +0530

    Check valid first name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace UserRegistrarionProblem
+﻿using UserRegistrationProblem;
+
+namespace UserRegistrarionProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome To User Registration Program. ");
+            
+            Console.WriteLine("Please enter the First Name : ");
+            string name = Console.ReadLine();
+            bool val = Pattern.validateName(name);
+            if (val)
+            {
+                Console.WriteLine("First Name is Valid.");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not Valid.");
+
+            }
 
         }
     }
